80c59c759ea99c5565e153cbc3b7b0835cc73e3e
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 13:27:13 +0900

    Create tyoukin1.user.js

---
+++ tyoukin1.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         ★超勤：カスタム再利用_累計コピー（独立版）
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=150*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=150*
+// @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/tyoukin1.js?v=1.0
+// @grant        none
+// @run-at       document-end
